483a714629eea4bd9b8bed66e7b6c8dda207097d
kshitish <kshitish182@gmail.com>
2019-04-29 11:26:31 +0545

    design initial push

---
+++ index.js
@@ -0,0 +1,12 @@
+let hamBurger = document.getElementById('hamburger');
+let burgerButton = document.getElementById('burgerButton');
+let 
+
+
+burgerButton.addEventListener('click', event => {
+	if(hamBurger.style.display === ""){
+		hamBurger.style.display = 'block';
+	}else if(hamBurger.style.display === 'block'){
+		hamBurger.style.display = "";
+	}
+});
