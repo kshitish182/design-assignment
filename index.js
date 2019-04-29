a04aff2ee1ba465e3bdfa3b60cdd2b394f6da2c3
kshitish <kshitish182@gmail.com>
2019-04-29 13:17:02 +0545

    additional changes made

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let hamBurger = document.getElementById('hamburger');
 let burgerButton = document.getElementById('burgerButton');
-
+let iconContainer = document.getElementsByClassName('icon-container');
+let innerList = document.getElementsByClassName('list-inner-element-container');
+let pgFirstRow = document.getElementsByClassName('pg-first-row');
 
 
 burgerButton.addEventListener('click', event => {
@@ -10,3 +12,20 @@ burgerButton.addEventListener('click', event => {
 		hamBurger.style.display = "";
 	}
 });
+
+
+	for(let i = 0; i< iconContainer.length; i++){
+		iconContainer[i].addEventListener('click', event =>{
+				console.log(innerList[i]);
+				if(innerList[i].style.display === "block"){
+					console.log('here');
+					innerList[i].style.display = "none";
+					pgFirstRow[i].style.marginBottom = "0px";
+
+				}else{
+					console.log('herealso')
+					innerList[i].style.display = "block";
+					pgFirstRow[i].style.marginBottom = "15px";
+				}
+		});
+	}
